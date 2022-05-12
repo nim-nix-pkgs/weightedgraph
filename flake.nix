f8e5ab05d5d11fc2c4713328ee854ac598e4301e
hugosenari <hugosenari@gmail.com>
2022-05-12 11:58:53 -0300

    chore: re index weightedgraph

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."weighted_graph-master".dir   = "master";
+  inputs."weighted_graph-master".owner = "nim-nix-pkgs";
+  inputs."weighted_graph-master".ref   = "master";
+  inputs."weighted_graph-master".repo  = "weighted_graph";
+  inputs."weighted_graph-master".type  = "github";
+  inputs."weighted_graph-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."weighted_graph-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
